0a508019d137d12170633d35e44b61ef5252df0d
PrintSTRhelloWorld <89860811+PrintSTRhelloWorld@users.noreply.github.com>
2024-01-08 21:32:57 +0100

    Scattegories.js

--- java.js
+++ java.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+let randomletterbtn =document.getElementById("randomletter")
+let randomletter=document.getElementById("generaterandomletter")
+randomletterbtn.addEventListener("click", function() {
+    let x = Math.floor(Math.random() * (90 - 65 + 1)) + 65;
+    let randomltr=String.fromCharCode(x);
+    randomletter.innerHTML=randomltr;
+})
+
