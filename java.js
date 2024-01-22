cb9f72f6d877e2c8affb1228620c9d923a7788c8
PrintSTRhelloWorld <89860811+PrintSTRhelloWorld@users.noreply.github.com>
2024-01-22 16:39:33 +0100

    Update Scattegories.js

--- java.js
+++ java.js
@@ -1,8 +1,7 @@
 let randomletterbtn =document.getElementById("randomletter")
 let randomletter=document.getElementById("generaterandomletter")
 randomletterbtn.addEventListener("click", function() {
-    let x = Math.floor(Math.random() * (90 - 65 + 1)) + 65;
+    let x = Math.floor(Math.random() * (90 -64)) + 65;
     let randomltr=String.fromCharCode(x);
-    randomletter.innerHTML=randomltr;
+    randomletter.innerHTML=`<label> ${randomltr} </label>  ` 
 })
-
